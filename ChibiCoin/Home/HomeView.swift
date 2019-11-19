75c287f0055871e492fb09a95f4a0af1167a327f
寺本真之 <shinji.teramoto@peers-management.com>
2019-11-19 15:00:41 +0900

    [add] home view

--- ChibiCoin/Home/HomeView.swift
+++ ChibiCoin/Home/HomeView.swift
@@ -10,7 +10,57 @@ import SwiftUI
 
 struct HomeView: View {
   var body: some View {
-    Text("ホーム")
+    ZStack {
+      Color(hue: 1.0, saturation: 0.014, brightness: 0.974)
+        .edgesIgnoringSafeArea(.all)
+      VStack {
+        VStack(spacing: 0) {
+          Spacer()
+          Text("チビコイン")
+            .font(.headline)
+            .padding()
+          Text("¥0")
+            .font(.title)
+          Spacer()
+          Divider()
+          Button(action: {}) { Text("チャージ(お手伝い)")
+          }
+          .foregroundColor(.red)
+          .padding()
+        }
+        .frame(width: 400, height: 200, alignment: .center)
+        .background(Color.white)
+        .padding()
+        HStack {
+          VStack {
+            VStack(spacing: 0) {
+              Image(systemName: "qrcode")
+                .resizable()
+                .frame(width: 40, height: 40, alignment: .center)
+                .padding(.top)
+              Text("コード決済")
+                .font(.headline)
+                .padding()
+            }
+            .frame(width: 200, height: 100, alignment: .center)
+            .background(Color.white)
+          }
+          VStack {
+            VStack(spacing: 0) {
+              Image(systemName: "barcode.viewfinder")
+              .resizable()
+              .frame(width: 40, height: 40, alignment: .center)
+              .padding(.top)
+              Text("読み取り")
+                .font(.headline)
+                .padding()
+            }
+            .frame(width: 200, height: 100, alignment: .center)
+            .background(Color.white)
+          }
+        }
+      }
+    }
   }
 }
 
